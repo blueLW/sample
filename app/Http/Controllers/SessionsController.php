c13d8d3ce2916219f478f1e2b655d8700aa277cf
LW <616458279@qq.com>
2019-08-12 07:16:31 +0000

    完成login和logout

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class SessionsController extends Controller
+{
+    //显示登陆页面
+    public function create(){
+        return view('sessions.create');
+    }
+
+    //验证登录
+
+    public function store(Request $request)
+    {
+       $credentials = $this->validate($request, [
+           'email' => 'required|email|max:255',
+           'password' => 'required'
+       ]);
+       if (Auth::attempt($credentials,$request->has('remember'))) {
+           // 登录成功后的相关操作
+           session()->flash('success', '欢迎回来！');
+           return redirect()->route('users.show', [Auth::user()]);
+       } else {
+           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+           return redirect()->back();
+       }
+       return;
+    }
+
+
+    public function destroy(){
+        Auth::logout();
+        session()->flash('success','您已成功推出!');
+        return redirect('login');
+    }
+}
